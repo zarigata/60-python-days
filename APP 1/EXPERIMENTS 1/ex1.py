4fda7e03e90fb63ed7fbf700e3eb1540b538d8b9
zarigata <Ferrary@12>
2023-06-21 03:09:30 -0300

    now with afuck you

--- APP 1/EXPERIMENTS 1/ex1.py
+++ APP 1/EXPERIMENTS 1/ex1.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("type add, show or exit:")
+    user_action = input("type add, show, edit or exit:")
     user_action = user_action.strip()
     match user_action:
         #ADD
@@ -13,10 +13,14 @@ while True:
             for item in todos:
                 item = item.title()
                 print(item)
+
+        case 'edit':
+            print ("10-4")
         #LEAVE
         case 'exit' | 'leave' | 'quit' | 'close':
             break
         #EVERITHNG ELSE
         case whatever:
             print("WHAT THE FUCK IS WRONG WITH YOU???? CHOSE ONE OF ABOVE BITCH")
+
 print("BYE BYE")
