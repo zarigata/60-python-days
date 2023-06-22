60593a9184f2e550f5094fe39813581a8a5b3a31
zarigata <vallestrauch@yahoo.com>
2023-06-22 01:10:47 -0300

    NEW PC

--- APP 1/main.py
+++ APP 1/main.py
@@ -1,10 +1,29 @@
-prontp = "Imprima uma coisa para anotar:"
-
 todos = []
 
 while True:
-    todo = input(prontp)
-    print(todo.title())
-    todos.append(todo)
-    print(todos)
+    user_action = input("type add, show, edit or exit:")
+    user_action = user_action.strip()
+    match user_action:
+        #ADD
+        case 'add':
+            todo =input("TODO:   ")
+            todos.append(todo)
+        #SHOW
+        case 'show' | 'display':
+            for item in todos:
+                item = item.title()
+                print(item)
+        #EDITOR OF TASKS
+        case 'edit':
+            number = int(input("Number of the todo to edit"))
+            number = number - 1
+            new_todos =input("Imput the new todo")
+            todos[number] = new_todos
+        #LEAVE
+        case 'exit' | 'leave' | 'quit' | 'close':
+            break
+        #EVERITHNG ELSE
+        case whatever:
+            print("WHAT THE FUCK IS WRONG WITH YOU???? CHOSE ONE OF ABOVE BITCH")
 
+print("BYE BYE")
